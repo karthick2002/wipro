bd7ce6978d6ea6f7bb79832a742ae12f7efa60ae
karthick2002 <43398475+karthick2002@users.noreply.github.com>
2022-02-17 13:14:18 +0530

    Delete e2e directory

--- e2e/com.sirius.library/reportInfo.js
+++
@@ -1,9 +0,0 @@
-let reportInfo = function () {
-
-    this.log = function (info) {
-        browser.controlFlow().execute(function(){
-        allure.createStep(info, () => {})();
-    });
-    } 
-};
-module.exports = new reportInfo();
